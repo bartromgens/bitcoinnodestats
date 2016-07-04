17bbf767cb70c4031efa938c510f5ffc2248355f
Bart Römgens <bart.romgens@gmail.com>
2016-07-04 13:33:41 +0200

    Create url parameters for bin size and date range

--- bitcoinnodestats/views.py
+++ bitcoinnodestats/views.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import pytz
 
 from django.views.generic import TemplateView
@@ -11,11 +11,22 @@ class HomeView(TemplateView):
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
+        bin_size_hour = 6
+        date_begin = datetime.utcnow() - timedelta(days=30)
+        date_end = datetime.now(tz=pytz.utc)
+
+        if 'date_begin' in self.request.GET:
+            date_begin = datetime.strptime(self.request.GET['date_begin'], "%Y-%m-%d")
+        if 'date_end' in self.request.GET:
+            date_end = datetime.strptime(self.request.GET['date_end'], "%Y-%m-%d")
+        if 'bin_size_hour' in self.request.GET:
+            bin_size_hour = int(self.request.GET['bin_size_hour'])
+
         context = super().get_context_data(**kwargs)
         context['stats'] = NodeStats(
-            bin_size_hour=24,
-            date_begin=datetime(2009, 1, 3),
-            date_end=datetime.now(tz=pytz.utc)
+            bin_size_hour=bin_size_hour,
+            date_begin=date_begin,
+            date_end=date_end
         )
         context['node'] = Node()
         return context
