210f3ecf81395ba20e313d034ae82c34d42ce713
Bart Römgens <bart.romgens@gmail.com>
2017-11-04 16:51:14 +0100

    Replace old Django setting MIDDLEWARE_CLASSES with MIDDLEWARE

--- bitcoinnodestats/settings.py
+++ bitcoinnodestats/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     # 'debug_toolbar',
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
